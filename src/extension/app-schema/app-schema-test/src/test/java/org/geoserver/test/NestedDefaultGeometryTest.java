1e022d9aa4a7d339d2cfa51d4edf44ef1582da28
Stefano Costa <ridethepenguin@gmail.com>
2018-03-09 17:18:23 +0100

    WIP: tests for first fully working implementation

--- src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/NestedDefaultGeometryTest.java
+++ src/extension/app-schema/app-schema-test/src/test/java/org/geoserver/test/NestedDefaultGeometryTest.java
@@ -14,18 +14,11 @@ import javax.imageio.ImageIO;
 
 import org.custommonkey.xmlunit.XpathEngine;
 import org.custommonkey.xmlunit.exceptions.XpathException;
-import org.geotools.image.test.ImageAssert;
-import org.geotools.util.URLs;
 import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 public class NestedDefaultGeometryTest extends AbstractAppSchemaTestSupport {
-
-    // GML 3.1 namespaces
-    private static final String STATIONS_PREFIX_GML31 = "st_gml31";
-    // GML 3.2 namespaces
-    private static final String STATIONS_PREFIX_GML32 = "st_gml32";
 
     // xpath engines used to check WFS responses
     private XpathEngine WFS11_XPATH_ENGINE;
@@ -60,25 +53,33 @@ public class NestedDefaultGeometryTest extends AbstractAppSchemaTestSupport {
         public void addContent() {
             // add GML 3.1 namespaces
             putNamespace(STATIONS_PREFIX_GML31, STATIONS_URI_GML31);
+            putNamespace(MEASUREMENTS_PREFIX_GML31, MEASUREMENTS_URI_GML31);
             // add GML 3.2 namespaces
             putNamespace(STATIONS_PREFIX_GML32, STATIONS_URI_GML32);
-            // add GML 3.1 feature type
+            putNamespace(MEASUREMENTS_PREFIX_GML32, MEASUREMENTS_URI_GML32);
+            // add GML 3.1 feature types
             Map<String, String> gml31Parameters = new HashMap<>();
             gml31Parameters.put("GML_PREFIX", "gml31");
             gml31Parameters.put("GML_NAMESPACE", "http://www.opengis.net/gml");
             gml31Parameters.put("GML_LOCATION", "http://schemas.opengis.net/gml/3.1.1/base/gml.xsd");
-            addStationFeatureType(STATIONS_PREFIX_GML31, "gml31", "stations", "nestedGeometryMappings/stations.xml", gml31Parameters);
-            // add GML 3.2 feature type
+            addStationFeatureType(STATIONS_PREFIX_GML31, "gml31", "Station", "stations", "nestedGeometryMappings/stations.xml", gml31Parameters);
+            addStationFeatureType(STATIONS_PREFIX_GML31, "gml31", "StationWithMeasurements",
+                    "stations", "nestedGeometryMappings/stationsWithMeasurements.xml",
+                    "measurements", "nestedGeometryMappings/measurements.xml", gml31Parameters);
+            addMeasurementFeatureType(MEASUREMENTS_PREFIX_GML31, "gml31", "measurements", "nestedGeometryMappings/measurements.xml", gml31Parameters);
+            // add GML 3.2 feature types
             Map<String, String> gml32Parameters = new HashMap<>();
             gml32Parameters.put("GML_PREFIX", "gml32");
             gml32Parameters.put("GML_NAMESPACE", "http://www.opengis.net/gml/3.2");
             gml32Parameters.put("GML_LOCATION", "http://schemas.opengis.net/gml/3.2.1/gml.xsd");
-            addStationFeatureType(STATIONS_PREFIX_GML32, "gml32", "stations", "nestedGeometryMappings/stations.xml", gml32Parameters);
+            addStationFeatureType(STATIONS_PREFIX_GML32, "gml32", "Station", "stations", "nestedGeometryMappings/stations.xml", gml32Parameters);
+            addStationFeatureType(STATIONS_PREFIX_GML32, "gml32", "StationWithMeasurements", "stations", "nestedGeometryMappings/stationsWithMeasurements.xml",
+                    "measurements", "nestedGeometryMappings/measurements.xml", gml32Parameters);
+            addMeasurementFeatureType(MEASUREMENTS_PREFIX_GML32, "gml32", "measurements", "nestedGeometryMappings/measurements.xml", gml32Parameters);
         }
-        
-        @Override
+
         protected void addStationFeatureType(String namespacePrefix, String gmlPrefix,
-                String mappingsName, String mappingsPath, Map<String, String> parameters) {
+                String featureType, String mappingsName, String mappingsPath, Map<String, String> parameters) {
             // create root directory
             File gmlDirectory = new File(TEST_ROOT_DIRECTORY, gmlPrefix);
             gmlDirectory.mkdirs();
@@ -91,41 +92,106 @@ public class NestedDefaultGeometryTest extends AbstractAppSchemaTestSupport {
             substituteParameters("/test-data/stations/data/stationsNestedGeometry.properties", parameters, stationsProperties);
             substituteParameters("/test-data/stations/schemas/stationsNestedGeometry.xsd", parameters, stationsSchema);
             // create station feature type
-            addFeatureType(namespacePrefix, String.format("Station_%s", gmlPrefix),
+            addFeatureType(namespacePrefix, String.format("%s_%s", featureType, gmlPrefix),
                     stationsMappings.getAbsolutePath(),
                     stationsProperties.getAbsolutePath(),
                     stationsSchema.getAbsolutePath());
         }
 
+        /**
+         * Helper method that will add the station feature type customizing it for the desired GML version.
+         */
+        protected void addStationFeatureType(String namespacePrefix, String gmlPrefix,
+                String stationsFeatureType, String stationsMappingsName,
+                String stationsMappingsPath, String measurementsMappingsName,
+                String measurementsMappingsPath, Map<String, String> parameters) {
+            // create root directory
+            File gmlDirectory = new File(TEST_ROOT_DIRECTORY, gmlPrefix);
+            gmlDirectory.mkdirs();
+            // add the necessary files
+            File stationsMappings = new File(gmlDirectory, String.format("%s_%s.xml", stationsMappingsName, gmlPrefix));
+            File stationsProperties = new File(gmlDirectory, String.format("stations_%s.properties", gmlPrefix));
+            File stationsSchema = new File(gmlDirectory, String.format("stations_%s.xsd", gmlPrefix));
+            File measurementsSchema = new File(gmlDirectory, String.format("measurements_%s.xsd", gmlPrefix));
+            // perform the parameterization
+            substituteParameters("/test-data/stations/" + stationsMappingsPath, parameters, stationsMappings);
+            substituteParameters("/test-data/stations/data/stationsNestedGeometry.properties", parameters, stationsProperties);
+            substituteParameters("/test-data/stations/schemas/stationsNestedGeometry.xsd", parameters, stationsSchema);
+            substituteParameters("/test-data/stations/schemas/measurementsNestedGeometry.xsd", parameters, measurementsSchema);
+            // create station feature type
+            addFeatureType(namespacePrefix, String.format("%s_%s", stationsFeatureType, gmlPrefix),
+                    stationsMappings.getAbsolutePath(),
+                    stationsProperties.getAbsolutePath(),
+                    stationsSchema.getAbsolutePath(),
+                    measurementsSchema.getAbsolutePath());
+        }
+
+        @Override
+        protected void addMeasurementFeatureType(String namespacePrefix, String gmlPrefix,
+                String mappingsName, String mappingsPath, Map<String, String> parameters) {
+            // create root directory
+            File gmlDirectory = new File(TEST_ROOT_DIRECTORY, gmlPrefix);
+            gmlDirectory.mkdirs();
+            // add the necessary files
+            File measurementsMappings = new File(gmlDirectory, String.format("%s_%s.xml", mappingsName, gmlPrefix));
+            File measurementsProperties = new File(gmlDirectory, String.format("measurements_%s.properties", gmlPrefix));
+            File measurementsSchema = new File(gmlDirectory, String.format("measurements_%s.xsd", gmlPrefix));
+            // perform the parameterization
+            substituteParameters("/test-data/stations/" + mappingsPath, parameters, measurementsMappings);
+            substituteParameters("/test-data/stations/data/measurementsNestedGeometry.properties", parameters, measurementsProperties);
+            substituteParameters("/test-data/stations/schemas/measurementsNestedGeometry.xsd", parameters, measurementsSchema);
+            // create measurements feature type
+            addFeatureType(namespacePrefix, String.format("Measurement_%s", gmlPrefix),
+                    measurementsMappings.getAbsolutePath(),
+                    measurementsProperties.getAbsolutePath(),
+                    measurementsSchema.getAbsolutePath());
+        }
+
     }
-    
+
     @Test
     public void testWfsGetFeature() throws XpathException {
-        Document document = getAsDOM("wfs?request=GetFeature&version=2.0&typename=st_gml32:Station_gml32");
-        
+        Document document = getAsDOM(
+                "wfs?request=GetFeature&version=2.0&typename=st_gml32:Station_gml32");
+
         try {
             String stationXpath = "/wfs:FeatureCollection/wfs:member/st_gml32:Station_gml32";
-            assertEquals(3, WFS20_XPATH_ENGINE.getMatchingNodes(stationXpath, document).getLength());
+            assertEquals(3,
+                    WFS20_XPATH_ENGINE.getMatchingNodes(stationXpath, document).getLength());
         } catch (Exception exception) {
             throw new RuntimeException("Error evaluating xpath.", exception);
         }
     }
-    
-    @Test
-    public void testWmsGetMap() throws IOException {
-        String getMapUrl = new StringBuilder().append("wms?request=GetMap").append("&")
-            .append("SRS=EPSG:4326").append("&")
-            .append("layers=st_gml32:Station_gml32").append("&")
-            .append("styles=Default").append("&")
-            .append("BBOX=-180,-90,180,90").append("&")
-            .append("WIDTH=256").append("&")
-            .append("HEIGHT=256").append("&")
-            .append("FORMAT=image/png")
-            .toString();
 
-        try (InputStream is = getBinary(getMapUrl)) {
+    @Test
+    public void testWmsGetMapNestedDefaultGeometry() throws IOException {
+        try (InputStream is = getBinary(buildGetMapUrl("st_gml32:Station_gml32"))) {
             BufferedImage imageBuffer = ImageIO.read(is);
             assertNotBlank("app-schema test getmap nested default geom", imageBuffer, Color.WHITE);
         }
+    }
+
+    @Test
+    public void testWmsGetMapDefaultGeometryInChainedFeature() throws IOException {
+        try (InputStream is = getBinary(buildGetMapUrl("st_gml32:StationWithMeasurements_gml32"))) {
+            BufferedImage imageBuffer = ImageIO.read(is);
+            assertNotBlank("app-schema test getmap nested default geom feature chaining",
+                    imageBuffer, Color.WHITE);
+        }
+    }
+
+
+    public String buildGetMapUrl(String layers) {
+        String getMapUrl = new StringBuilder().append("wms?request=GetMap").append("&")
+                .append("SRS=EPSG:4326").append("&")
+                .append("layers=").append(layers).append("&")
+                .append("styles=Default").append("&")
+                .append("BBOX=-10,-10,10,10").append("&")
+                .append("WIDTH=256").append("&")
+                .append("HEIGHT=256").append("&")
+                .append("FORMAT=image/png")
+                .toString();
+
+        return getMapUrl;
     }
 }
